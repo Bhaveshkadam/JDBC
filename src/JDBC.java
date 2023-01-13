9fa3dc934814e0bf54f7036807759fcce5e17d5d
Bhaveshkadam <bhaukadam508@gmail.com>
2023-01-13 20:01:22 +0530

    update db name and refactor code and add count function

--- src/JDBC.java
+++ src/JDBC.java
@@ -1,19 +1,33 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.*;
+import java.sql.*;
+import java.io.Reader;
+
 
 public class JDBC {
+    private Connection conn;
+    private String TABLE_NAME = "Question_Set";
+
+    // TODO : refactor table name
     public Connection connect(String dbname, String user, String pass) {
-        Connection conn = null;
+        // TODO: Make a data config driven
+        // TODO: Use log framework
+//        Connection conn = null;
         try {
             Class.forName("org.postgresql.Driver");
             conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, user, pass);
             if (conn != null) {
-                System.out.println("Connection Established");
+                System.out.println("\n Connection Established");
+                createTable(conn, TABLE_NAME);
             } else {
-                System.out.println("Connection Failed");
+                System.out.println("\n Connection Failed");
             }
+            Statement stmt = conn.createStatement();
+            String query = String.format("select count(*) from %s", TABLE_NAME);
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            int count = rs.getInt(1);
+            System.out.println("\n Number of question in the Question_Set table: "+count +"\n" );
+
 
         } catch (Exception e) {
             System.out.println(e);
@@ -21,10 +35,11 @@ public class JDBC {
         return conn;
     }
 
-//    For create table in Database.
-    public void createTable(Connection conn, String table_name) {
+    //    For create table in Database.
+    private void createTable(Connection conn, String table_name) {
         Statement statement;
         try {
+            // TODO: Refactor query to support to create if not exist.
             String query = "create table " + table_name + "(question_id SERIAL,question varchar(200),answer varchar(200),primary key(question_id));";
             statement = conn.createStatement();
             statement.executeUpdate(query);
@@ -34,11 +49,15 @@ public class JDBC {
         }
     }
 
-//    Insert data into table
-    public void insertQuestion(Connection conn, String table_name, String question, String answer) {
+    public void setTABLE_NAME(String TABLE_NAME) {
+        this.TABLE_NAME = TABLE_NAME;
+    }
+
+    //    Insert data into table
+    public void insertQuestion(String question, String answer) {
         Statement statement;
         try {
-            String query = String.format("insert into %s(question,answer) values('%s','%s');", table_name, question, answer);
+            String query = String.format("insert into %s(question,answer) values('%s','%s');", TABLE_NAME, question, answer);
             statement = conn.createStatement();
             statement.executeUpdate(query);
             System.out.println("Row Inserted");
@@ -47,12 +66,12 @@ public class JDBC {
         }
     }
 
-//    read all data from table
-    public void readQuestion(Connection conn, int count) {
+    //    read all data from table
+    public void readQuestion(int count) {
         Statement statement;
         ResultSet rs = null;
         try {
-            String query = String.format("select * from Question_Answer LIMIT %s", count);
+            String query = String.format("select * from Question_Set LIMIT %s", count);
             statement = conn.createStatement();
             rs = statement.executeQuery(query);
             while (rs.next()) {
@@ -66,12 +85,12 @@ public class JDBC {
         }
     }
 
-//    search data from table
-    public void getQuestionById(Connection conn, String table_name, int question_id) {
+    //    search data from table
+    public void getQuestionById(int question_id) {
         Statement statement;
         ResultSet rs = null;
         try {
-            String query = String.format("select * from %s where question_id= '%s'", table_name, question_id);
+            String query = String.format("select * from %s where question_id= '%s'", TABLE_NAME, question_id);
             statement = conn.createStatement();
             rs = statement.executeQuery(query);
             while (rs.next()) {
@@ -84,5 +103,8 @@ public class JDBC {
             System.out.println(e);
         }
     }
+    // TODO : Get count of all the questions.
+    // TODO : Create a function to seed question and answer
+    // TODO : Think about reading question and answer from CSV file
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
         JDBC db = new JDBC();
-        Connection conn = db.connect("questionanswer", "postgres", "qwerty1201");
-        db.createTable(conn,"Question_Answer");
-        db.insertQuestion(conn,"Question_Answer","what is the year of independence day?","1947");
-        db.insertQuestion(conn,"Question_Answer","Who among the following was the Prime Minister of Britain at the time of Independence?","Clement Attlee");
-        db.readQuestion(conn, 5);
-        db.getQuestionById(conn, "Question_Answer", 1);
+        // TODO: see all naming conventions
 
+        Connection conn = db.connect("question_answer", "postgres", "qwerty1201");
+//        db.setTABLE_NAME("QuestionSet");
+
+//        db.insertQuestion("what is the year of independence day?","1947");
+//        db.insertQuestion("Who among the following was the Prime Minister of Britain at the time of Independence?","Clement Attlee");
+        db.readQuestion(50);
+        for (int i = 0; i < 4; i++) {
+            db.getQuestionById(i);
+        }
     }
-
 }
